f2b21f5eb7aacc0aed2321b70642da3c5afe6e58
Victor <86551854+devDalys@users.noreply.github.com>
2022-03-14 15:19:04 +0300

    Fixed bug with get data from firebase

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -7,33 +7,36 @@ import 'firebase/compat/firestore';
 import {Avatar, Button, Container, Grid, TextField} from "@mui/material";
 import {useCollectionData} from "react-firebase-hooks/firestore";
 import {Context} from "../index";
+import {getFirestore, collection, orderBy} from 'firebase/firestore'
 import Loader from "./Loader";
 
-const Chat =  () => {
+const Chat = () => {
+    const db = getFirestore();
     const auth = getAuth(firebase.firebaseapp);
-    const {firestore} = useContext(Context);
     const [user] = useAuthState(auth);
     const [value, setValue] = useState('');
-    const [messages, loading] = useCollectionData(
-         firebase.firestore().collection('messages').orderBy('CreatedAt').get());
-    const sendMessage = async () =>{
+    const sendMessage = async () => {
         await firebase.firestore().collection('messages').add({
-            uid:user.uid,
-            displayName:user.displayName,
-            photoURL:user.photoURL,
-            text:value,
-            createdAt:firebase.firestore.FieldValue.serverTimestamp()
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            text: value,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
         })
         setValue('');
     }
-    if(loading) {<Loader/>}
+
+    const [messages, loading] = useCollectionData(collection(db, 'messages'));
+    // if(!loading){console.log(messages.sort((a,b) => a.createdAt.nanoseconds > b.createdAt.nanoseconds));}
+    if (loading) return <Loader/>
+
     return (
         <Container>
             <Grid container
                   justifyContent={"center"}
                   style={{height: window.innerHeight - 50, marginTop: '20px'}}>
                 <div style={{width: '100%', height: '70vh', border: '1px solid gray', overflowY: 'auto'}}>
-                    {messages?.map(message =>
+                    {messages.map(message =>
                         <div style={{
                             margin: 10,
                             border: user.uid === message.uid ? '2px solid green' : '2px dashed red',
@@ -55,7 +58,8 @@ const Chat =  () => {
                     alignItems={'flex-end'}
                     style={{width: '100%'}}
                 >
-                    <TextField  value={value} onChange={e => setValue(e.target.value)} fullWidth variant={"outlined"}></TextField>
+                    <TextField value={value} onChange={e => setValue(e.target.value)} fullWidth
+                               variant={"outlined"}></TextField>
                     <Button onClick={sendMessage} style={{marginTop: '5px'}} variant={'outlined'}>Отправить</Button>
                 </Grid>
             </Grid>
